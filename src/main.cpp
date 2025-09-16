97794b2e1ca62640121be3271a8726c2e250ae8f
Tilenology <tilensimencthedev@gmail.com>
2025-09-16 11:01:35 +0200

    SDL start rectangle drawing

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,58 @@
+#include <SDL.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello";
+#define SCREEN_WIDTH 1080
+#define SCREEN_HEIGHT 720
+
+int WinMain() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << '\n';
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+        "SDL2 Beginner Example",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        0);
+    if (!window) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << '\n';
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(
+        window, -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!renderer) {
+        std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << '\n';
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    while (running) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) running = false;
+            if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE) running = false;
+        }
+
+        // Clear with a dark background
+        SDL_SetRenderDrawColor(renderer, 30, 30, 60, 255);
+        SDL_RenderClear(renderer);
+
+        // Draw an orange rectangle
+        SDL_SetRenderDrawColor(renderer, 255, 140, 0, 255);
+        SDL_Rect r{100, 100, 300, 200};
+        SDL_RenderFillRect(renderer, &r);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
